0e786c431d96a0269681618002e0b5e59e73840b
Conrado Lanusse <conrado.lanusse@gmail.com>
2023-06-14 22:29:59 -0300

    Clase 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Conrado Lanusse THE BEST</h1>
+      <h1>Conrado Lanusse</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
